73d8b7c0d38f7cfe55daa6cd3ae83f734d28f0b9
brigidamirella <brigidamirella@hotmail.com>
2021-12-30 19:18:25 -0300

    criação da home

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,151 +1,70 @@
 <template>
-  <v-container>
+<v-app id="home">
+  <v-container id="top">
     <v-row class="text-center">
       <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
+       <v-img
+          ><v-img
+             max-height="500"
+            max-width="300"
+            src="../assets/solinftec.png"
+          ></v-img
+        ></v-img>
+         
       </v-col>
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
+
+      <v-col class="mb-4" cols="5">
+        <br><br><br>
+        <h1 class="display-2 font-weight-bold mb-3">Seja Bem-Vindo</h1>
 
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
+          Estamos apresentando 5 projetos de
+          <br />consumo de API'S :)
+          <br>Conheça nosso repositório,
+          <br>temos temas diversos!
+          <br>
         </p>
+
+        <div>
+          <a href="https://github.com/brigidamirella/desafio-vue" target="_blank"><img height="40" width="150" src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white"></a>
+        </div>
+        <v-spacer></v-spacer>
       </v-col>
+      <v-col cols="7">
+        <v-img
+          ><v-img
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
+            max-height="1000"
+            max-width="1000"
+            src="../assets/api.png"
+          ></v-img
+        ></v-img>
       </v-col>
     </v-row>
-  </v-container>
+   
+  </v-container> 
+  </v-app>
 </template>
+
 
 <script>
   export default {
     name: 'HelloWorld',
-
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }),
+methods : {
+        btnClick: function() {
+            window.open("https://github.com/brigidamirella/desafio-vue");
+        }
+}
   }
 </script>
+<style scoped>
+p{
+  font-size: 30px;
+  font-family: 'Times New Roman', Times, serif;
+}
+#home{
+  background-color: rgb(215, 221, 202);
+  margin-top: -14px;
+}
+</style>
