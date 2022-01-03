5bf046a37f57500f4b3f1cd21bf7d9d7bed202a9
Juliano Appel <juhappel@hotmail.com>
2022-01-03 16:05:07 -0300

    projeto final

--- src/views/Juliano.vue
+++ src/views/Juliano.vue
@@ -1,36 +1,16 @@
 <template>
-  <v-app class="page">
+  <v-app class="page" style="margin-top: 0px;">
     <v-card class="mx-auto" tile>
       <div>
         <p class="father">
-          Assista aos melhores momentos dos últimos jogos da sua Liga favorita!
+          Acompanhe sua Liga favorita! <p class="son">
+          Melhores momentos dos últimos jogos, análises jogo a jogo, tabelas, e muito mais!
         </p>
         <span class="espaco"></span>
         <v-row>
-          <v-col col="6">
-            <div class="pl">
-              <v-btn rounded color="#004D40" dark @click="getLeague1()">Premier League (ENG)</v-btn>
-            </div>
-            <span class="espaco"></span>
-            <div>
-              <v-btn rounded color="#004D40" dark @click="getLeague2()">La Liga (ESP)</v-btn>
-            </div>
-            <span class="espaco"></span>
-            <div>
-              <v-btn rounded color="#004D40" dark @click="getLeague3()">Serie A (ITA)</v-btn>
-            </div>
-          </v-col>
-          <v-col col="6">
-            <div>
-              <v-btn rounded color="#004D40" dark @click="getLeague4()">Bundesliga (GER)</v-btn>
-            </div>
-            <span class="espaco"></span>
-            <div>
-              <v-btn rounded color="#004D40" dark @click="getLeague5()">Ligue 1 (FRA)</v-btn>
-            </div>
-            <span class="espaco"></span>
-            <div>
-              <v-btn rounded color="#004D40" dark @click="getLeague6()">Série A (BRA)</v-btn>
+          <v-col cols="6" class="mx-auto">
+            <div class="botoes">
+              <v-btn v-for="(item, i) in items" :key="i" rounded color="#004D40" dark v-text="item.liga" @click="getLeague(item.text)" style="margin-bottom: 10px; margin-right: 10px;margin-left: 10px;"></v-btn>
             </div>
           </v-col>
         </v-row>
@@ -44,6 +24,16 @@
         </div>
       </div>
     </div>
+    <v-alert
+        dense
+        type="warning"
+        v-show="msgShow"
+        v-model="msgAlert"
+        width="auto"
+        class="mx-auto mt-5"
+        transition="slide-x-reverse-transition"
+        >{{ msgAlert }}</v-alert
+      >
   </v-app>
 </template>
 
@@ -52,66 +42,53 @@ export default {
   data: () => ({
     image: "",
     exibir: false,
+    msgShow: false,
+    msgAlert: "",
+    items: [
+      { 
+        text: "ENGLAND: Premier League",
+        liga: "Premier League (ING)"
+      },
+      { 
+        text: "SPAIN: La Liga",
+        liga: "La Liga (ESP)"
+      },
+      {
+        text: "ITALY: Serie A",
+        liga: "Serie A (ITA)"
+       },
+      {
+        text: "GERMANY: Bundesliga",
+        liga: "Bundesliga (ALE)"
+      },
+      {
+        text: "FRANCE: Ligue 1",
+        liga: "Ligue 1 (FRA)"
+      },
+      {
+        text: "BRAZIL: Serie A",
+        liga: "Série A (BRA)"
+      },
+    ],
   }),
   methods: {
-    async getLeague1() {
+    async getLeague(competicao) {
       const response = await fetch("https://www.scorebat.com/video-api/v3/");
       const retorno = await response.json();
-      var matchId1 = 0;
-      if (retorno.response[matchId1].competition === "ENGLAND: Premier League") {
-        this.image = retorno.response[matchId1].competitionUrl;
+      var matchId = retorno.response.findIndex(element => {
+        return element.competition === competicao;
+      });
+      console.log(matchId);
+      if (matchId != -1) {
+        this.image = retorno.response[matchId].competitionUrl;
         this.exibir = true;
-        matchId1++;
-      }
-    },
-    async getLeague2() {
-      const response = await fetch("https://www.scorebat.com/video-api/v3/");
-      const retorno = await response.json();
-      var matchId2 = 0;
-      if (retorno.response[matchId2].competition === "SPAIN: La Liga") {
-        this.image = retorno.response[matchId2].competitionUrl;
-        this.exibir = true;
-        matchId2++;
-      }
-    },
-    async getLeague3() {
-      const response = await fetch("https://www.scorebat.com/video-api/v3/");
-      const retorno = await response.json();
-      var matchId2 = 0;
-      if (retorno.response[matchId2].competition === "ITALY: Serie A") {
-        this.image = retorno.response[matchId2].competitionUrl;
-        this.exibir = true;
-        matchId2++;
-      }
-    },
-    async getLeague4() {
-      const response = await fetch("https://www.scorebat.com/video-api/v3/");
-      const retorno = await response.json();
-      var matchId2 = 0;
-      if (retorno.response[matchId2].competition === "GERMANY: Bundesliga") {
-        this.image = retorno.response[matchId2].competitionUrl;
-        this.exibir = true;
-        matchId2++;
-      }
-    },
-    async getLeague5() {
-      const response = await fetch("https://www.scorebat.com/video-api/v3/");
-      const retorno = await response.json();
-      var matchId2 = 0;
-      if (retorno.response[matchId2].competition === "FRANCE: Ligue 1") {
-        this.image = retorno.response[matchId2].competitionUrl;
-        this.exibir = true;
-        matchId2++;
-      }
-    },
-    async getLeague6() {
-      const response = await fetch("https://www.scorebat.com/video-api/v3/");
-      const retorno = await response.json();
-      var matchId2 = 0;
-      if (retorno.response[matchId2].competition === "BRAZIL: Serie A") {
-        this.image = retorno.response[matchId2].competitionUrl;
-        this.exibir = true;
-        matchId2++;
+      } else {
+        this.exibir = false;
+        this.msgShow = true;
+        this.msgAlert = "Não houveram jogos recentes nessa competição!";
+        setTimeout(() => {
+          this.msgShow = false;
+        }, 3000);
       }
     },
   },
@@ -119,6 +96,7 @@ export default {
 </script>
 
 <style scoped>
+
 .page {
   background-image: url(../assets/fut.png);
   background-repeat: no-repeat;
@@ -128,16 +106,18 @@ export default {
   font-size: 30px;
   padding-top: 30px;
 }
-.espaco {
-  padding-left: 5px;
+.son {
+  font-size: 20px;
+  padding-bottom: 20px;
 }
 .mx-auto {
-  width: 80%;
+  width: 90%;
   height: relative;
   padding-bottom: 40px;
+  align-items: center;
 }
 .wrapper {
-  width: 80%;
+  width: 90%;
   height: 100%;
   margin: 0 auto;
   background: #ccc;
