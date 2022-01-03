5cb78b3180d00b528c1ee45ce6ce42417953d8cf
Feliphe Silva <feliphevs90@outlook.com>
2022-01-03 14:11:32 -0300

    clima finalizado

--- src/views/Feliphe.vue
+++ src/views/Feliphe.vue
@@ -1,18 +1,25 @@
+
+
 <template>
   <v-card
     class="mx-auto"
     max-width="400"
     
   >
-  <v-card-title class="text-center">
-      
+  <v-spacer></v-spacer>
+
+  <div class="center">
+    
+    
         <h1 class="display-1">SolinfteClima</h1>
-      
-    </v-card-title>
+    </div>
+ 
   <v-text-field 
+
+        
         v-model="cep"
         label="Digite o CEP" 
-        v-on:blur="getCEP" 
+        @keyup.enter="getCEP" 
         v-mask="['#####-###']"
         
         
@@ -33,12 +40,14 @@
         <v-col
           class="text-h2"
           cols="6"
+          v-model="temp"
+
         >
-          23&deg;C
+          {{temp}}
         </v-col>
         <v-col cols="6">
           <v-img
-            src="https://cdn.vuetifyjs.com/images/cards/sun.png"
+            :src="icone"
             alt="Sunny image"
             width="92"
           ></v-img>
@@ -50,132 +59,97 @@
       <v-list-item-icon>
         <v-icon>mdi-send</v-icon>
       </v-list-item-icon>
-      <v-list-item-subtitle>23 km/h</v-list-item-subtitle>
+      <v-list-item-subtitle v-model="vento">{{vento}}</v-list-item-subtitle>
     </v-list-item>
 
     <v-list-item>
       <v-list-item-icon>
-        <v-icon>mdi-cloud-download</v-icon>
+        <v-icon>mdi-cloud</v-icon>
       </v-list-item-icon>
-      <v-list-item-subtitle>48%</v-list-item-subtitle>
+      <v-list-item-subtitle v-model="nuvens">{{nuvens}}</v-list-item-subtitle>
     </v-list-item>
 
-    <v-slider
-      v-model="time"
-      :max="6"
-      :tick-labels="labels"
-      class="mx-4"
-      ticks
-    ></v-slider>
+    <v-list-item>
+      <v-list-item-icon>
+        <v-icon>mdi-water-alert</v-icon>
+      </v-list-item-icon>
+      <v-list-item-subtitle v-model="umidade">{{umidade}}</v-list-item-subtitle>
+    </v-list-item>
 
-    <v-list class="transparent">
-      <v-list-item
-        v-for="(item, i) in forecast"
-        :key="i"
-      >
-        <v-list-item-title>{{ item.day }}</v-list-item-title>
-
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-subtitle class="text-right">
-          {{ item.temp }}
-        </v-list-item-subtitle>
-      </v-list-item>
-    </v-list>
-
-    <v-divider></v-divider>
-
-    <v-card-actions>
-      <!-- <v-btn text>
-        Full Report
-      </v-btn> -->
-    </v-card-actions>
   </v-card>
 </template>
 
 <script>
+
+
   export default {
     data () {
     
       return {
           temp: "",
+          vento: "",
+          nuvens: "",
+          umidade: "",
+          icone: "",
           diaHoraAtual: "",
           cep: "",
           cidade:"",
           cidadeEstado: "",
-          
-        labels: ['DO', 'SE', 'TE', 'QU', 'QU', 'SE', 'SA'],
-        time: 0,
-        forecast: [
-          { day: 'Tuesday', icon: 'mdi-white-balance-sunny', temp: '24\xB0/12\xB0' },
-          { day: 'Wednesday', icon: 'mdi-white-balance-sunny', temp: '22\xB0/14\xB0' },
-          { day: 'Thursday', icon: 'mdi-cloud', temp: '25\xB0/15\xB0' },
-        ],
       }
     },
+
     methods: {
-        async getCEP(){
+  
+      async getCEP(){
         const cepFormatado = this.cep.replace("-", "");
-        
-
-
-      
-        
-        if (cepFormatado.length === 8) {
-            try {
-            const response = await fetch(
+          if (cepFormatado.length == 8) {
+             try {
+              const response = await fetch(
                 `https://viacep.com.br/ws/${cepFormatado}/json/`,
-                );
-            const json = await response.json();
-            console.log(json);
-            
-            
-            const cidade = json.localidade;
-            this.cidadeEstado = json.localidade + " - " + json.uf;
-
-            const dias = new Array(
-              'Dom','Seg','Ter','Qua','Qui','Sex','Sáb'
               );
+              const json = await response.json();
+              console.log(json);
 
+              this.cidade = json.localidade;
+              this.cidadeEstado = json.localidade + " - " + json.uf;
 
-            const dataAtual = new Date();
-            this.diaHoraAtual = dias[dataAtual.getDay()]
-              + "," + dataAtual.getHours() 
-              + ":" + dataAtual.getMinutes();
-
-              
-              
-
-            
-            
-
-            
-            
-
-            
-            
-            const jsonClimas = await fetch(
-              `http://api.weatherstack.com/forecast?access_key=ac6ac11102fd4590c487fd46b481bc84&query=${cidade}`,
-               );
-
-            const jsonFinal = await jsonClimas.json();       
-                          
-            this.time = 4;
-            // console.log("getForecast => ", jsonFinal.current.temperature);
-            console.log("jsonFinal => ", jsonFinal);
-            console.log("getForecast =>", jsonFinal);
+              this.getData();
+              this.getTemp();
           } catch (error) {
-              console.log("CEP inválido do catch", error);
+              console.log("Erro de API de CEP", error);
           }
-        } else {
-            console.log("CEP inválido do else");
-        }     
-        
-        
+          } else {
+            console.log("CEP inválido");
+        }          
       },
 
+      getData() {
+        const dias = new Array('Dom','Seg','Ter','Qua','Qui','Sex','Sáb');
+        const dataAtual = new Date();
+        this.diaHoraAtual = dias[dataAtual.getDay()] 
+          + ", "
+          + dataAtual.getHours()
+          + ":"
+          + dataAtual.getMinutes();  
+      },
+
+      async getTemp() {
+        try {
+          const response = await fetch(
+            `http://api.weatherstack.com/forecast?access_key=8d13acdbe0c36f9dfca4fa6de1843e93&query=${this.cidade}`,
+          );
+
+          const jsonClima = await response.json();       
+          
+          this.temp = `${jsonClima.current.temperature}°C`;
+          this.vento = `Vento: ${jsonClima.current.wind_speed} km/h`;
+          this.nuvens = `Nebulosidade: ${jsonClima.current.cloudcover}%`;
+          this.umidade = `Umidade do ar: ${jsonClima.current.humidity}%`;
+          this.icone = jsonClima.current.weather_icons[0];
+        } catch (error) {
+          console.log("Erro de API de clima");
+        }       
+      },
 
     }
   }
