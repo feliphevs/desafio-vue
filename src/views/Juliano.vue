db23538719443080f9dc35824d5a6a515d0e602a
Feliphe Silva <feliphevs90@outlook.com>
2022-01-03 16:14:11 -0300

    correcoes pagina juliano

--- src/views/Juliano.vue
+++ src/views/Juliano.vue
@@ -1,56 +1,136 @@
 <template>
-  <v-app>
-    <v-card width="500" class="mx-auto mt-5">
-      <v-select
-        :items="[
-          'Premier League',
-          'La Liga',
-          'Serie A (ITA)',
-          'Bundesliga',
-          'Ligue 1',
-          'Série A (BRA)',
-        ]"
-        label="Choose your League"
-      >
-        <template v-slot:item="{ item, attrs, on }">
-          <v-list-item v-bind="attrs" v-on="on">
-            <v-list-item-title :id="attrs" v-text="item"></v-list-item-title>
-          </v-list-item>
-          <v-btn color="success" @click="getLeague"> Get videos! </v-btn>
-        </template>
-      </v-select>
+  <v-app class="page" style="margin-top: 0px">
+    <v-card class="mx-auto" tile>
+      <div>
+        <p class="father">Acompanhe sua Liga favorita!</p>
+        <p class="son">
+          Melhores momentos dos últimos jogos, análises jogo a jogo, tabelas, e
+          muito mais!
+        </p>
+        <span class="espaco"></span>
+        <v-row>
+          <v-col cols="6" class="mx-auto">
+            <div class="botoes">
+              <v-btn
+                v-for="(item, i) in items"
+                :key="i"
+                rounded
+                color="#004D40"
+                dark
+                v-text="item.liga"
+                @click="getLeague(item.text)"
+                style="
+                  margin-bottom: 10px;
+                  margin-right: 10px;
+                  margin-left: 10px;
+                "
+              ></v-btn>
+            </div>
+          </v-col>
+        </v-row>
+      </div>
     </v-card>
-    <div class="wrapper" v-if="this.exibir">
-      <div class="h_iframe">
-        <!-- a transparent image is preferable -->
-        <img class="ratio" src="http://placehold.it/16x9" />
-        <iframe :src="image" frameborder="50" allowfullscreen></iframe>
+    <div>
+      <div class="wrapper" v-if="exibir">
+        <div class="h_iframe">
+          <img class="ratio" src="http://placehold.it/16x9" />
+          <iframe :src="image" frameborder="50" allowfullscreen></iframe>
+        </div>
       </div>
     </div>
+    <v-alert
+      dense
+      type="warning"
+      v-show="msgShow"
+      v-model="msgAlert"
+      width="auto"
+      class="mx-auto mt-5"
+      transition="slide-x-reverse-transition"
+      >{{ msgAlert }}</v-alert
+    >
   </v-app>
 </template>
 
 <script>
 export default {
   data: () => ({
-    bars: [{ class: "" }],
     image: "",
     exibir: false,
+    msgShow: false,
+    msgAlert: "",
+    items: [
+      {
+        text: "ENGLAND: Premier League",
+        liga: "Premier League (ING)",
+      },
+      {
+        text: "SPAIN: La Liga",
+        liga: "La Liga (ESP)",
+      },
+      {
+        text: "ITALY: Serie A",
+        liga: "Serie A (ITA)",
+      },
+      {
+        text: "GERMANY: Bundesliga",
+        liga: "Bundesliga (ALE)",
+      },
+      {
+        text: "FRANCE: Ligue 1",
+        liga: "Ligue 1 (FRA)",
+      },
+      {
+        text: "BRAZIL: Serie A",
+        liga: "Série A (BRA)",
+      },
+    ],
   }),
   methods: {
-    async getLeague() {
+    async getLeague(competicao) {
       const response = await fetch("https://www.scorebat.com/video-api/v3/");
       const retorno = await response.json();
-      this.image = retorno.response[0].matchviewUrl;
-      this.exibir = true;
+      var matchId = retorno.response.findIndex((element) => {
+        return element.competition === competicao;
+      });
+      console.log(matchId);
+      if (matchId != -1) {
+        this.image = retorno.response[matchId].competitionUrl;
+        this.exibir = true;
+      } else {
+        this.exibir = false;
+        this.msgShow = true;
+        this.msgAlert = "Não houveram jogos recentes nessa competição!";
+        setTimeout(() => {
+          this.msgShow = false;
+        }, 3000);
+      }
     },
   },
 };
 </script>
 
 <style scoped>
+.page {
+  background-image: url(../assets/fut.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.father {
+  font-size: 30px;
+  padding-top: 30px;
+}
+.son {
+  font-size: 20px;
+  padding-bottom: 20px;
+}
+.mx-auto {
+  width: 90%;
+  height: relative;
+  padding-bottom: 40px;
+  align-items: center;
+}
 .wrapper {
-  width: 100%;
+  width: 90%;
   height: 100%;
   margin: 0 auto;
   background: #ccc;
